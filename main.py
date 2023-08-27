46d4f8dc84fed5e1f920fb71c05eef0d5b104f99
h2 <wnhg2czh@gmail.com>
2023-08-27 20:47:35 +0800

    Change frontend framework
    Change Jinja2 within this repo for frontend -> external react typescript frontend

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 # uvicorn main:app --reload
 import os
 import openai
-from typing import Optional
-from fastapi import FastAPI, Form, Request
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
-openai.api_key = os.environ.get("OPENAI_API_KEY", "")
-templates = Jinja2Templates(directory="templates/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # adjust this in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-async def read_root(request: Request, result: Optional[str] = None):
-    return templates.TemplateResponse("index.html", {"request": request, "result": result})
+openai.api_key = os.environ.get("OPENAI_API_KEY", "")
+
+class Prompt(BaseModel):
+    user_prompt: str
 
 @app.post("/")
-async def generate_reply(request: Request, user_prompt: str = Form(...)):
+async def generate_reply(prompt: Prompt):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": generate_prompt(user_prompt)}
+            {"role": "user", "content": generate_prompt(prompt.user_prompt)}
         ]
     )
     generated_result = response['choices'][0]['message']['content'].strip()
-    return templates.TemplateResponse("index.html", {"request": request, "generated_result": generated_result})
+    return {"generated_result": generated_result}
 
 def generate_prompt(user_prompt: str) -> str:
     return user_prompt.capitalize()
