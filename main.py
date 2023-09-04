e80f4c970860ec06d813f468321d28d698607da7
h2 <wnhg2czh@gmail.com>
2023-09-04 21:45:04 +0800

    Add checks and validation
    - API key check
    - origin to hard code current frontend url
    - try catch for generating reply

--- main.py
+++ main.py
@@ -1,39 +1,47 @@
 # uvicorn main:app --reload
 import os
 import openai
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # TODO: adjust this in production
+    allow_origins=["http://localhost:3000/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-openai.api_key = os.environ.get("OPENAI_API_KEY", "")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+if not OPENAI_API_KEY:
+    raise ValueError("OpenAI API Key must be set.")
+
+openai.api_key = OPENAI_API_KEY
 
 class Prompt(BaseModel):
     user_prompt: str
 
 initial_chat_log = [{"role": "system", "content": "You are a helpful assistant."}]
-chat_logs = initial_chat_log
+chat_logs = initial_chat_log # TODO: still global, consider using database/session storage
 
 @app.post("/")
 async def generate_reply(prompt: Prompt):
+    user_input = generate_prompt(prompt.user_prompt)
     chat_logs.append({
         "role": "user",
-        "content": generate_prompt(prompt.user_prompt)
+        "content": user_input
     })
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=chat_logs
-    )
-    generated_result = response['choices'][0]['message']['content'].strip()
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=chat_logs
+        )
+        generated_result = response['choices'][0]['message']['content'].strip()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error interacting with OpenAI API.")
 
     chat_logs.append({
         "role": "assistant",
@@ -52,4 +60,5 @@ async def get_chat_logs():
     return {"chat_logs": chat_logs}
 
 def generate_prompt(user_prompt: str) -> str:
-    return user_prompt.capitalize()
+    return user_prompt.capitalize()  # TODO: add more validation/sanitization here
+
